778acadcf9c3520992e9b233e9c2061cd05ebac3
AARON M <asmarcks@gmail.com>
2019-12-08 18:22:12 -0600

    variables

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("hey");
+
+//variables//
+
+var firstName = 'aaron';
+
+var lastName = 'marcks';
+
+var age = 37;
+
+var fullAge = true;
+console.log(firstName, lastName, age, fullAge);
+
+var job;
+
+job = 'head shop';
+console.log('works at a ' + job);
+
+//breaking rules//
+
+// var 3years = 3;
+// var aaron/marcks = 'aaron marcks';
+// var function = "aaron";
+// var if = 'aaron';
