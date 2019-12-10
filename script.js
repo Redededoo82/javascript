225b877e055f0151cb35ff66c4b0cefdc9b543c4
AARON M <asmarcks@gmail.com>
2019-12-09 20:16:06 -0600

    objects and properties and methods

--- script.js
+++ script.js
@@ -169,26 +169,26 @@
 
 //ternary operator and switch statements//
 
-var firstName = 'aaron';
-var age = 37;
+// var firstName = 'aaron';
+// var age = 37;
 
-age >= 21 ? console.log(firstName + ' drinks beer') : console.log(firstName + ' drinks juice');
+// age >= 21 ? console.log(firstName + ' drinks beer') : console.log(firstName + ' drinks juice');
 
-var drink = age>=21 ? 'beer' : 'juice';
-console.log(drink);
+// var drink = age>=21 ? 'beer' : 'juice';
+// console.log(drink);
 
-//switch statement//
+// //switch statement//
 
-var job = 'tranny';
-switch(job){
-    case 'head shop operator': console.log(firstName + ' sells bongs');
-    break;
-    case 'web developer': console.log(firstName +' develops websites');
-    break;
-    case 'tranny': console.log(firstName +' is a tranny')
-    break;
-    default: console.log('does nothing');
-}
+// var job = 'tranny';
+// switch(job){
+//     case 'head shop operator': console.log(firstName + ' sells bongs');
+//     break;
+//     case 'web developer': console.log(firstName +' develops websites');
+//     break;
+//     case 'tranny': console.log(firstName +' is a tranny')
+//     break;
+//     default: console.log('does nothing');
+// }
 
 /**************************************************************************************************************************************/
 
@@ -196,10 +196,188 @@ switch(job){
 //falsy values :  undefined, null, 0, '', NaN
 //truthy values : all non falsy values
 
-var height;
-height = 55;
-if (height){
-    console.log('variable is defined')
-}else{
-    console.log('variable is not defined')
+// var height;
+// height = 55;
+// if (height){
+//     console.log('variable is defined')
+// }else{
+//     console.log('variable is not defined')
+// }
+
+/******************************************************************************************************************************** */
+
+//functions//
+// var firstName;
+// firstName = 'aaron', 'russell', 'keith';
+
+// function calculateAge(birthYear) {
+//     return 2019 - birthYear;
+// }
+
+// var ageAaron = calculateAge(1982);
+// var ageRussell = calculateAge(1980);
+// var ageKeith = calculateAge(1984);
+// console.log(ageAaron, ageRussell, ageKeith);
+
+// function yearsUntilRetirement(birthYear, firstName) {
+//     var age = calculateAge(birthYear);
+//     var retirement = 65 - age;
+//     console.log(firstName + ' retires in ' + retirement + ' years');
+// }
+
+// yearsUntilRetirement(1982, 'aaron');
+// yearsUntilRetirement(1980, 'russell');
+// yearsUntilRetirement(1984, 'keith');
+
+/************************************************************************************************* */
+
+//function statements and expressions//
+
+// var whatDoYouDo = function (job, firstName) {
+//     switch(job){
+//         case 'head shop operator':
+//             return firstName + ' sells bongs';
+//         case 'wizard':
+//             return firstName + ' casts spells'
+//         case 'liberal fruitcake':
+//             return firstName + ' crams beliefs down throats'
+//         default: 
+//         return firstName + ' does something else'
+//     }
+// }
+// console.log(whatDoYouDo('wizard', 'russell'));
+// console.log(whatDoYouDo('head shop operator', 'aaron'));
+
+/**************************************************************************************************************************** */
+
+//arrays//
+
+// var names = ['aaron', 'russell', 'keith'];
+// var years = new Array(1982, 1980, 1984);
+
+// names[3] = 'scott';
+// years[3] = 1989
+
+// console.log(names);
+// console.log(years);
+// console.log(names.length);
+
+/******************************************************************************************************************************** */
+
+//coding challenge 3 (i skipped 2 for some reason)//
+
+function calculateTip(billAmount) {
+    var percentage;
+    if (billAmount < 50){
+        percentage = .2;
+    }else if(billAmount >= 50 && billAmount < 200){
+        percentage =.15;
+    }else{
+        percentage = .1;
+    };
+    return percentage * billAmount;
+};
+
+// console.log(calculateTip(120));
+
+var bills = [124, 48, 268];
+var tips = [
+    calculateTip(bills[0]),
+    calculateTip(bills[1]),
+    calculateTip(bills[2]),
+];
+
+var finalValues = [
+    bills[0] + tips[0],
+    bills[1] + tips[1],
+    bills[2] + tips[2],
+]
+// console.log(tips, finalValues);
+function displayInfo() {
+    console.log(bills, tips, finalValues) ;
 }
+/******************************************************************************************************************* */
+
+//objects and properties//
+
+// var aaron = {
+//     firstName:'aaron',
+//     lastName: 'marcks',
+//     birthYear: 1982,
+//     family:['russell', 'keith', 'scott'],
+//     job: 'head shop operator',
+//     isMarried: false
+// };
+// // console.log(aaron.family[1]);
+// // console.log(aaron['lastName']);
+// var x = 'birthYear';
+// // console.log(aaron[x]);
+
+// aaron.job = 'wizard';
+// aaron['isMarried'] = true;
+// // console.log(aaron);
+
+// var keith = new Object();
+// keith.firstName = 'keith';
+// keith.birthYear = 1984;
+// // console.log(keith);
+
+/************************************************************************************************************************* */
+
+//objects and methods//
+
+var aaron = {
+    firstName:'aaron',
+    lastName: 'marcks',
+    birthYear: 1989,
+    family:['russell', 'keith', 'scott'],
+    job: 'head shop operator',
+    isMarried: false,
+    calcAge: function(){
+        return this.age = 2019 - this.birthYear;
+    }
+};
+
+function ageOfAaron() {
+    console.log(aaron.calcAge());
+    
+}
+
+/*******************************************************************************************************************88 */
+
+//coding challenge 4//
+
+//create objects with full name, mass, and height//
+//add method to each object that calculates b.m.i. Save bmi to object and return it from the method//
+var mark = {
+    fullName: 'mark johnson',
+    height: 6.3,
+    mass: 200,
+    calcBmi: function () {
+        return this.mass / (this.height * this.height);
+    }
+}
+
+var john = {
+    fullName: 'john johnston',
+    height: 5.5,
+    mass: 100,
+    calcBmi: function () {
+        return this.mass / (this.height * this.height);
+    }
+}
+//console log who has the highest bmi
+console.log(mark.calcBmi(), john.calcBmi());
+
+if(mark.calcBmi() > john.calcBmi()){
+    console.log('marks bmi is higher with...' + mark.calcBmi() + ' john had ' + john.calcBmi())
+}else{
+    console.log('johns bmi is higher with...' + john.calcBmi() + ' mark had ' + mark.calcBmi())
+};
+
+
+
+
+
+
+
